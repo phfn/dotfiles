0062a8a0a7f632a6e941ef722dad6b7227a8b86d
Paul Hoffmann <phfn@phfn.de>
2021-03-04 20:29:05 +0100

    added init script

---
+++ init.sh
@@ -0,0 +1,4 @@
+echo "source ~/dotfiles/.vimrc" >> ~/.vimrc
+mkdir -p ~/.config/nvim
+echo "source ~/dotfiles/init.vim" >> ~/.config/nvim/init.vim
+echo "source ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
