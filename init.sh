ce532498dbc3711f1d353c2616c6bc538fdb986d
Paul Hoffmann <paul.hoffmann@novatec-gmbh.de>
2021-03-23 00:40:26 +0100

    added coc-settings.json sync

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ echo "source ~/dotfiles/.vimrc" >> ~/.vimrc
 mkdir -p ~/.config/nvim
 echo "source ~/dotfiles/init.vim" >> ~/.config/nvim/init.vim
 echo "source ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
+cp ~/dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
+cp ~/dotfiles/vim/coc-settings.json ~/.vim/coc-settings.json
