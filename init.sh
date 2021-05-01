927faec98b9bcdca926fe6d167fab1ccff74ed31
Paul Hoffmann <paul.hoffmann@novatec-gmbh.de>
2021-05-01 11:00:32 +0200

    Add nvim-qt settings

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 echo "source ~/dotfiles/.vimrc" >> ~/.vimrc
 mkdir -p ~/.config/nvim
 echo "source ~/dotfiles/init.vim" >> ~/.config/nvim/init.vim
+echo "source ~/dotfiles/ginit.vim" >> ~/.config/nvim/ginit.vim
 echo "source ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
 cp ~/dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
 cp ~/dotfiles/vim/coc-settings.json ~/.vim/coc-settings.json
