36e800d42edc6e3ee69048ece0e994e3a6a42ec1
Paul Hoffmann <phfn@phfn.de>
2026-01-04 13:46:12 +0100

    Add Telescope whichkey

--- vim/plugins.lua
+++ vim/plugins.lua
@@ -47,7 +47,6 @@ require("lazy").setup({
 		}
 	},
 	{'https://github.com/hrsh7th/nvim-cmp',
-		lazy = false,
 		dependencies = {
 			{'https://github.com/petertriho/cmp-git',
 				dependencies = {"https://github.com/nvim-lua/plenary.nvim"},
@@ -112,37 +111,80 @@ require("lazy").setup({
 				colorscheme gruvbox
 			]])
 		end,
-		lazy = false
 	},
 	{'https://github.com/kyazdani42/nvim-tree.lua',
 		lazy = false,
+		opts = { },
+		init = function() vim.cmd([[
+			let g:nvim_tree_quit_on_open = 1
+		]])end,
 		keys = {
-			{ "<leader>t<leader", ":NvimTreeToggle", desc = "NeoTree" },
+			{'<leader>t<leader>', "<cmd>NvimTreeToggle<cr>", desc = "Live grep"},
+			{'<leader>tt', ":Telescope find_files", desc = "Find file"},
 		},
-		opts = { }
 	},
 	{'https://github.com/tpope/vim-fugitive',
 		config = function() vim.cmd([[
-			map <leader>ga :w<CR>:Git add %<CR>
-			map <leader>gA :Git add --patch %<CR>
-			map <leader>gc :Git commit<CR>
-			map <leader>gC :Git commit --amend<CR>
-			map <leader>gs :G<CR>
-			map <leader>gS :Git status<CR>
-			map <leader>gp :Git push<CR>
-			map <leader>gP :Git push --force
-			map <leader>gdd :vert Gdiffsplit<CR>
-			map <leader>gd1 :vert Gdiffsplit HEAD~1<CR>
-			map <leader>gd :vert Gdiffsplit
-			map <leader>gD :Gvdiffsplit!<CR>
-			map <leader>get :diffget<CR>
-			map <leader>gut :diffget<CR>
-			map <leader>gf :diffget //2<CR>
-			map <leader>gj :diffget //3<CR>
 			au FileType fugitive set spell
 			au FileType fugitive set spelllang=en
 		]]) end,
+		keys = { 
+			{ "<leader>ga",  ":w<CR>:Git add %<CR>",        desc = "Git add current file" },
+			{ "<leader>gA",  ":Git add --patch %<CR>",      desc = "Git add patch current file" },
+			{ "<leader>gc",  ":Git commit<CR>",             desc = "Git commit" },
+			{ "<leader>gC",  ":Git commit --amend<CR>",     desc = "Git amend commit" },
+			{ "<leader>gs",  ":G<CR>",                      desc = "Git status (fugitive)" },
+			{ "<leader>gp",  ":Git push<CR>",               desc = "Git push" },
+			{ "<leader>gP",  ":Git push --force<CR>",       desc = "Git push force" },
+			{ "<leader>gdd", ":vert Gdiffsplit<CR>",        desc = "Git vertical diff" },
+			{ "<leader>gd1", ":vert Gdiffsplit HEAD~1<CR>", desc = "Git diff HEAD~1" },
+			{ "<leader>gd",  ":vert Gdiffsplit<CR>",        desc = "Git diff (vert)" },
+			{ "<leader>gD",  ":Gvdiffsplit!<CR>",           desc = "Git diff (vert, bang)" },
+			{ "<leader>get", ":diffget<CR>",                desc = "Git diffget" },
+			{ "<leader>gut", ":diffget<CR>",                desc = "Git diffget" },
+			{ "<leader>gf",  ":diffget //2<CR>",            desc = "Git diffget //2" },
+			{ "<leader>gj",  ":diffget //3<CR>",            desc = "Git diffget //3" },
+			{ "<leader>gs",  ":G<CR>",                      desc = "Show git status" },
+		}
 	},
+	{ 'nvim-telescope/telescope.nvim', 
+		version = '0.2.x',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		keys = {
+			{'<leader>f<leader>', ':Telescope git_files<CR>', desc = "Find file in git"},
+			{'<leader>fv', ':Telescope git_files cwd=~/dotfiles/<CR>', desc = "Find files in dotfiles"},
+			{'<leader>fb', ':Telescope buffers<CR><ESC>k', desc = "Find a Buffer"},
+			{'<leader>F', ':Telescope find_files<CR>', desc = "Find a file in pwd"},
+			{'<leader>fr', ':Telescope oldfiles<CR>', desc = "Find a recently edited file"},
+			{'<leader>fg', ':Telescope live_grep<CR>', desc = "Find a file base on grep"},
+			{'<leader>ft', ':Telescope builtin<CR>', desc = "Find a Telescope function"},
+			{'<leader>fh', ':Telescope help_tags<CR>', desc = "Find Help"},
+			{'<leader>fs', '<cmd>Telescope grep_string<CR><ESC>', desc = "Find by Grep String"},
+			{'<leader>ff', ':Telescope lsp_document_symbols<CR>', desc = "Find  LSP Symbold"},
+			{'<leader>fF', ':Telescope lsp_workspace_symbols<CR>', desc = "Find LSP Workspace Symbol"},
+			{'z=', '<cmd>Telescope spell_suggest<CR><ESC>', desc = "Suggest spell"},
+			{'""', '<cmd>Telescope registers<CR>', desc = "Show Clipboards"}
+		},
+	},
+	{ "folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{ "<leader>?", function() require("which-key").show({ global = false }) end, desc = "Buffer Local Keymaps (which-key)", },
+			{ "<leader>g", group = "Git" },
+		},
+	},
+	{'chiendo97/intellij.vim',
+		config = false
+	},
+	{'doums/darcula',
+		config = false
+	},
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
