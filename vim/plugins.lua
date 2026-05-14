97889fd0769a5a21bcfc1546a40106d14474d064
Paul Hoffmann <phfn@phfn.de>
2026-05-14 14:24:14 +0200

    Let lazy not notify a thing

--- vim/plugins.lua
+++ vim/plugins.lua
@@ -151,9 +151,9 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "gruvbox" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
 
 -- Set up lspconfig
